68443c5212d12525e33e0c8c76fa15a659f92446
Arroneous2 <thumpinbassbeat@gmail.com>
2024-06-25 15:01:10 -0500

    show foods

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,8 +1,33 @@
-<script></script>
+<script>
+import FoodsIndex from "./FoodsIndex.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    FoodsIndex,
+  },
+  data: function () {
+    return {
+      foods: [],
+    };
+  },
+  created() {
+    this.handleIndexFoods();
+  },
+  methods: {
+    handleIndexFoods: function () {
+      axios.get("http://localhost:3000/foods.json").then((response) => {
+        console.log("foods index", response);
+        this.foods = response.data;
+      });
+    },
+  },
+};
+</script>
 
 <template>
   <main>
-    <h1>Welcome to Vue!</h1>
+    <FoodsIndex v-bind:foods="foods" />
   </main>
 </template>
 
---
+++ src/components/FoodsIndex.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: {
+    foods: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All Food</h1>
+    <div v-for="food in foods" v-bind:key="food.id">
+      <h2>{{ food.name }}</h2>
+      <h3>{{ food.ethnicity }}</h3>
+      <h4>{{ food.time }}</h4>
+    </div>
+  </div>
+</template>
+
+<style></style>
